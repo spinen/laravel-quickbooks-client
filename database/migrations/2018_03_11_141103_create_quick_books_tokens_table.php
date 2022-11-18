33e6ea38a2426dbf19d0ca3d54aa403127b4fc0e
Jimmy Puckett <jimmy.puckett@spinen.com>
2022-11-18 09:25:45 -0500

    Use anonymous class migration format

--- database/migrations/2018_03_11_141103_create_quick_books_tokens_table.php
+++ database/migrations/2018_03_11_141103_create_quick_books_tokens_table.php
@@ -5,8 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-class CreateQuickBooksTokensTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,8 +14,10 @@ class CreateQuickBooksTokensTable extends Migration
     public function up()
     {
         Schema::create('quickbooks_tokens', function (Blueprint $table) {
-            $user_id_type = DB::getSchemaBuilder()
-                              ->getColumnType('users', 'id') === 'bigint' ? 'unsignedBigInteger' : 'unsignedInteger';
+            $user_id_type =
+                DB::getSchemaBuilder()->getColumnType('users', 'id') === 'bigint'
+                    ? 'unsignedBigInteger'
+                    : 'unsignedInteger';
 
             $table->bigIncrements('id');
             $table->{$user_id_type}('user_id');
@@ -28,10 +29,11 @@ class CreateQuickBooksTokensTable extends Migration
 
             $table->timestamps();
 
-            $table->foreign('user_id')
-                  ->references('id')
-                  ->on('users')
-                  ->onDelete('cascade');
+            $table
+                ->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
         });
     }
 
@@ -44,4 +46,4 @@ class CreateQuickBooksTokensTable extends Migration
     {
         Schema::dropIfExists('quickbooks_tokens');
     }
-}
+};
